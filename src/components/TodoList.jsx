445380e631fbf553e0e19af28d0592e3bf99855a
Luiz Felipe <luizfelipewarmling@gmail.com>
2023-06-09 17:32:04 -0300

    [feat] Bulk list com modal

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -17,13 +17,65 @@ import Button from "@mui/material/Button";
 import Chip from "@mui/material/Chip";
 import Grid from "@mui/material/Grid";
 import { MdCheckBox, MdCheckBoxOutlineBlank } from "react-icons/md";
+import { green, grey, pink } from "@mui/material/colors";
+import {
+  Bookmark,
+  BookmarkBorder,
+  Cancel,
+  Check,
+  Delete,
+  Numbers,
+  PriorityHigh,
+  RadioButtonChecked,
+  RadioButtonUnchecked,
+} from "@mui/icons-material";
+import { Box, Divider, Typography } from "@mui/material";
 
 export default function TodoList({ todos = [], setTodos }) {
   const [show, setShow] = useState(false);
   const [record, setRecord] = useState(null);
+  const [selectedIds, setSelectedIds] = useState([]);
+  const [bulkPriority, setBulkPriority] = useState("");
+  const [showPriorityDialog, setShowPriorityDialog] = useState(false);
+  const [showBulkActions, setShowBulkActions] = useState(false);
 
   const handleClose = () => {
     setShow(false);
+  };
+  const handleBulkActionsOpen = () => {
+    setShowBulkActions(true);
+  };
+
+  const handleClosePriorityDialog = () => {
+    setShowPriorityDialog(false);
+  };
+
+  const handleBulkPriorityChange = (e) => {
+    setBulkPriority(e.target.value);
+  };
+
+  const handleBulkPriorityUpdate = () => {
+    const updatedTodos = todos.map((t) => {
+      if (selectedIds.includes(t.id)) {
+        return { ...t, priority: Number(bulkPriority) };
+      }
+      return t;
+    });
+
+    const updatePromises = selectedIds.map((id) => {
+      return handleUpdate(id, { priority: Number(bulkPriority) });
+    });
+
+    Promise.all(updatePromises)
+      .then(() => {
+        setTodos(updatedTodos);
+        setSelectedIds([]);
+        setBulkPriority("");
+        handleClosePriorityDialog(); // Fechar o diálogo após a atualização
+      })
+      .catch(() => {
+        alert("Something went wrong");
+      });
   };
 
   const handleDelete = (id) => {
@@ -34,6 +86,22 @@ export default function TodoList({ todos = [], setTodos }) {
           return t.id !== id;
         });
         setTodos(newTodos);
+      })
+      .catch(() => {
+        alert("Something went wrong");
+      });
+  };
+
+  const handleBulkDelete = () => {
+    const deletePromises = selectedIds.map((id) => {
+      return axios.delete(`/api/todos/${id}/`);
+    });
+
+    Promise.all(deletePromises)
+      .then(() => {
+        const newTodos = todos.filter((t) => !selectedIds.includes(t.id));
+        setTodos(newTodos);
+        setSelectedIds([]);
       })
       .catch(() => {
         alert("Something went wrong");
@@ -58,6 +126,14 @@ export default function TodoList({ todos = [], setTodos }) {
       });
   };
 
+  const handleToggleTodo = (id) => {
+    if (selectedIds.includes(id)) {
+      setSelectedIds(selectedIds.filter((selectedId) => selectedId !== id));
+    } else {
+      setSelectedIds([...selectedIds, id]);
+    }
+  };
+
   const renderListItem = (t) => {
     let priorityLabel = "";
     let chipColor = "";
@@ -74,21 +150,38 @@ export default function TodoList({ todos = [], setTodos }) {
     }
 
     return (
-      <ListItem
-        key={t.id}
-        className="d-flex justify-content-between align-items-center"
-      >
+      <ListItem className="d-flex justify-content-between align-items-center">
         <Grid container alignItems="center" spacing={2}>
           <Grid item>
             <Checkbox
+              checked={selectedIds.includes(t.id)}
+              onChange={() => {
+                handleToggleTodo(t.id);
+              }}
+              icon={<RadioButtonUnchecked />}
+              checkedIcon={<RadioButtonChecked />}
+            />
+          </Grid>
+          <Grid item>
+            <Divider orientation="vertical" flexItem />
+          </Grid>
+          <Grid item>
+            <Checkbox
+              icon={<BookmarkBorder />}
+              checkedIcon={<Bookmark />}
+              sx={{
+                "& .MuiSvgIcon-root": { fontSize: 28 },
+                color: green[800],
+                "&.Mui-checked": {
+                  color: green[600],
+                },
+              }}
               checked={t.completed}
               onChange={() => {
                 handleUpdate(t.id, {
                   completed: !t.completed,
                 });
               }}
-              icon={<MdCheckBoxOutlineBlank />}
-              checkedIcon={<MdCheckBox />}
             />
           </Grid>
           <Grid item>
@@ -126,6 +219,15 @@ export default function TodoList({ todos = [], setTodos }) {
     );
   };
 
+  const handleSelectAll = () => {
+    if (selectedIds.length === todos.length) {
+      setSelectedIds([]);
+    } else {
+      const allIds = todos.map((t) => t.id);
+      setSelectedIds(allIds);
+    }
+  };
+
   const handleChange = (e) => {
     setRecord({
       ...record,
@@ -150,8 +252,139 @@ export default function TodoList({ todos = [], setTodos }) {
     .filter((t) => t.completed)
     .sort((a, b) => b.priority - a.priority);
 
+  const handleBulkComplete = (completed) => {
+    const updatedTodos = todos.map((t) => {
+      if (selectedIds.includes(t.id)) {
+        return { ...t, completed: completed };
+      }
+      return t;
+    });
+
+    const updatePromises = selectedIds.map((id) => {
+      return handleUpdate(id, { completed: completed });
+    });
+
+    Promise.all(updatePromises)
+      .then(() => {
+        setTodos(updatedTodos);
+        setSelectedIds([]);
+      })
+      .catch(() => {
+        alert("Something went wrong");
+      });
+  };
+
   return (
     <div>
+      <div className="mb-2 mt-4">
+        <Box display="flex" alignItems="center">
+          <Typography variant="subtitle1" sx={{ mr: 1 }}>
+            Selecionar todos:
+          </Typography>
+          <Checkbox
+            checked={selectedIds.length === todos.length}
+            indeterminate={
+              selectedIds.length > 0 && selectedIds.length < todos.length
+            }
+            onChange={handleSelectAll}
+            icon={<RadioButtonUnchecked />}
+            checkedIcon={<RadioButtonChecked />}
+          />
+        </Box>
+        <Button
+          variant="contained"
+          onClick={handleBulkActionsOpen}
+          disabled={selectedIds.length === 0}
+          color="primary"
+          sx={{ bgcolor: grey[600] }}
+          endIcon={<EditIcon />}
+        >
+          Editar em Massa
+        </Button>
+
+        <Dialog
+          open={showBulkActions}
+          onClose={() => setShowBulkActions(false)}
+        >
+          <DialogTitle>Ações em Massa</DialogTitle>
+          <DialogContent>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Button
+                  variant="contained"
+                  onClick={() => handleBulkComplete(true)}
+                  disabled={selectedIds.length === 0}
+                  color="success"
+                  endIcon={<Check />}
+                  fullWidth
+                >
+                  Marcar selecionados como completos
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  variant="contained"
+                  onClick={() => handleBulkComplete(false)}
+                  disabled={selectedIds.length === 0}
+                  color="secondary"
+                  endIcon={<Cancel />}
+                  fullWidth
+                >
+                  Marcar selecionados como incompletos
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  variant="contained"
+                  onClick={handleBulkDelete}
+                  disabled={selectedIds.length === 0}
+                  color="error"
+                  endIcon={<Delete />}
+                  fullWidth
+                >
+                  Excluir selecionados
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  variant="contained"
+                  onClick={() => setShowPriorityDialog(true)}
+                  disabled={selectedIds.length === 0}
+                  color="warning"
+                  endIcon={<PriorityHigh />}
+                  fullWidth
+                >
+                  Editar prioridade
+                </Button>
+              </Grid>
+            </Grid>
+          </DialogContent>
+        </Dialog>
+      </div>
+      <Dialog open={showPriorityDialog} onClose={handleClosePriorityDialog}>
+        <DialogTitle>Editar Prioridade em Massa</DialogTitle>
+        <DialogContent>
+          <TextField
+            label="Nova Prioridade"
+            type="number"
+            value={bulkPriority}
+            onChange={handleBulkPriorityChange}
+            margin="dense"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClosePriorityDialog}>Cancelar</Button>
+          <Button
+            onClick={handleBulkPriorityUpdate}
+            variant="contained"
+            autoFocus
+          >
+            Salvar
+          </Button>
+        </DialogActions>
+      </Dialog>
+
       <div className="mb-2 mt-4">
         Tarefas incompletas ({sortedIncompleteTodos.length})
       </div>
@@ -160,6 +393,7 @@ export default function TodoList({ todos = [], setTodos }) {
         Tarefas Completas ({sortedCompletedTodos.length})
       </div>
       <List>{sortedCompletedTodos.map(renderListItem)}</List>
+
       <Dialog open={show} onClose={handleClose}>
         <DialogTitle>Edit Todo</DialogTitle>
         <DialogContent>
